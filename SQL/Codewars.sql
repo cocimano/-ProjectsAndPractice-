ee363cbb9b2183ae816c70e16bda18b9a6549e4d
FedericoJCocimano <89555@sistemas.frc.utn.edu.ar>
2024-01-28 20:56:20 -0300

    sql-codewars

--- SQL/Codewars.sql
+++ SQL/Codewars.sql
@@ -209,6 +209,36 @@ INNER JOIN job j ON p.id = j.people_id
 GROUP BY j.job_title
 ORDER BY average_salary DESC
 
+/*
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+/*
+SQL Basics: Simple PIVOTING data WITHOUT CROSSTAB
+This kata is inspired by SQL Basics: Simple PIVOTING data by matt c.
+You need to build a pivot table WITHOUT using CROSSTAB function. Having two tables products and details you need to select a pivot table of products with counts of details occurrences (possible details values are ['good', 'ok', 'bad'].
+Results should be ordered by product's name.
+Model schema for the kata is:
+
+your query should return table with next columns
+name
+good
+ok
+bad
+Compare your table to the expected table to view the expected results.
+
+Solution:
+*/
+SELECT p.name,
+(SELECT COUNT(d.detail) FROM details d WHERE detail = 'good' AND p.id = d.product_id ) as good,
+(SELECT COUNT(d.detail) FROM details d WHERE detail = 'ok' AND p.id = d.product_id) as ok,
+(SELECT COUNT(d.detail) FROM details d WHERE detail = 'bad' AND p.id = d.product_id) as bad
+FROM products p INNER JOIN details d ON p.id = d.product_id
+GROUP BY p.name, p.id
+ORDER BY p.name
+
+/*
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
 
 
 
