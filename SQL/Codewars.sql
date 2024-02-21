ffff83181f58ed8387161854a40252e8db276591
FedericoJCocimano <89555@sistemas.frc.utn.edu.ar>
2024-02-21 16:19:54 -0300

    Practice SQL

--- SQL/Codewars.sql
+++ SQL/Codewars.sql
@@ -436,6 +436,47 @@ FROM CROSSTAB (
 /*
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 */
+/*
+SQL Basics: Simple VIEW
+For this challenge you need to create a VIEW. This VIEW is used by a sales store to give out vouches to members who have spent over $1000 in departments that have brought in more than $10000 total ordered by the members id. The VIEW must be called members_approved_for_voucher then you must create a SELECT query using the view.
+Tables and relationship below:
+
+	resultant table schema
+•	id
+•	name
+•	email
+•	total_spending
+
+Solution:
+*/
+CREATE VIEW members_approved_for_voucher
+AS
+SELECT m.id
+	,m.name
+	,m.email
+	,SUM(p.price) AS total_spending
+FROM members m
+INNER JOIN sales s ON s.member_id = m.id
+RIGHT JOIN products p ON s.product_id = p.id
+RIGHT JOIN departments d ON s.department_id = d.id
+WHERE 10000 <= (
+		SELECT SUM(p2.price)
+		FROM products p2
+		INNER JOIN sales s2 ON p2.id = s2.product_id
+		INNER JOIN departments d2 ON s2.department_id = d2.id
+		WHERE d2.id = s.department_id
+		)
+GROUP BY 1
+	,2
+	,3
+HAVING SUM(p.price) > 1000
+ORDER BY m.id;
+
+SELECT *
+FROM members_approved_for_voucher;
+/*
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
 
 
 
